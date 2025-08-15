437427c1d9b522b44cc3dc0366ab23347d0e2804
richard williams <richard.williams5697@yahoo.com>
2025-08-15 16:48:08 +0900

    watch list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,4 +10,19 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
