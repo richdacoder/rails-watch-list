3baadcc4f90f6f1780874ad41a1cc478124324ef
richard williams <richard.williams5697@yahoo.com>
2025-08-15 16:56:14 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
   get 'bookmarks/new'
   get 'bookmarks/create'
   get 'bookmarks/destroy'
   resources :lists, only: [:index, :show, :new, :create ] do
-  resources :bookmarks, only: [:new, :create, :destroy]
-end
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
